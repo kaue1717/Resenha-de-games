86f59420dd0ac07c187c68d867b0ded364da15f8
Programação-java <kaue171717@gmail.com>
2025-05-21 21:53:54 -0300

    Todas ás alterações feitas por mim

--- RESENHA-BOLADA-DOS-GAMES/Resenhabolada.cs
+++ RESENHA-BOLADA-DOS-GAMES/Resenhabolada.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Jogo
+{
+    public string Nome { get; set; }
+    public string Genero { get; set; }
+    public string Plataforma { get; set; }
+    public string AnoLancamento { get; set; }
+    public bool EBOM { get; private set; }
+    public bool ERUIM { get; private set; }
+    public bool EMAHOMENO { get; private set; }
+
+    // Construtor dentro da classe
+    public Jogo(string nome, string genero, string plataforma, string anoLancamento)
+    {
+        Nome = nome;
+        Genero = genero;
+        Plataforma = plataforma;
+        AnoLancamento = anoLancamento;
+    }
+
+    public void Avaliar(bool EBOM = true, bool ERUIM = false)
+    {
+        this.EBOM = EBOM;
+        this.ERUIM = ERUIM;
+        this.EMAHOMENO = EBOM && ERUIM; // Se for bom e ruim ao mesmo tempo, será "mais ou menos"
+    }
+
+    public void ExibirAvaliação()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Gênero: {Genero}");
+        Console.WriteLine($"Plataforma: {Plataforma}");
+        Console.WriteLine($"Ano de Lançamento: {AnoLancamento}");
+        Console.WriteLine($"Avaliação: {(EBOM ? "Bom" : ERUIM ? "Ruim" : EMAHOMENO ? "É MAIS OU MENOS..." : "Sem avaliação")}");
+    }
+
+
+    public void ObterOpininao()
+    {
+        Console.WriteLine("Qual a sua opinião sobre o game?");
+        string opiniao = Console.ReadLine();
+
+        Console.WriteLine("Que nota vc daria pro jogo de 0 a 10?");
+        string suanota = Console.ReadLine();
+
+        Console.WriteLine($"Você disse: {opiniao} e deu a nota: {suanota}");//$ usado para interpolar
+        //variaveis dentro de strings//
+    }
+
+}
